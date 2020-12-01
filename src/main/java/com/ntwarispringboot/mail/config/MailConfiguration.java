58ae7524ab4ac0d3062c704b6ca0a47d06ba0ac3
ntwari-egide <ntwariegide2@gmail.com>
2020-12-01 21:19:04 +0200

    Fit configured the Global configuration port, username,password from mail trap service provider

---
+++ src/main/java/com/ntwarispringboot/mail/config/MailConfiguration.java
@@ -0,0 +1,50 @@
+package com.ntwarispringboot.mail.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MailConfiguration {
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+
+    public MailConfiguration(String host, int port, String username, String password) {
+        this.host = host;
+        this.port = port;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
