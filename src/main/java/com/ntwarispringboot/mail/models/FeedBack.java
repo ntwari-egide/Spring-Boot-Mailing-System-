34dac667dd3602b238ac5f9b82e695c06b6002fc
ntwari-egide <ntwariegide2@gmail.com>
2020-12-01 21:24:19 +0200

    Fit added added the feedback component with corresponding properties

---
+++ src/main/java/com/ntwarispringboot/mail/models/FeedBack.java
@@ -0,0 +1,37 @@
+package com.ntwarispringboot.mail.models;
+
+public class FeedBack {
+    private String name;
+    private String email;
+    private String feedback;
+
+    public FeedBack(String name, String email, String feedback) {
+        this.name = name;
+        this.email = email;
+        this.feedback = feedback;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+}
