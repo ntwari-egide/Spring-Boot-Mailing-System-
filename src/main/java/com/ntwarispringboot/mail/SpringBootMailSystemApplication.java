8f148f1dfee72fea64a34ad0337c815e2d73e286
ntwari-egide <ntwariegide2@gmail.com>
2020-12-01 21:05:07 +0200

    Fit initializing the system of mailing in spring boot

---
+++ src/main/java/com/ntwarispringboot/mail/SpringBootMailSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ntwarispringboot.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMailSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMailSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntwarispringboot/mail/SpringBootMailSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntwarispringboot.mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMailSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
